185f80f470175c5b55183938477658cfc3f28a4e
SRamzanA <ramzansmajov14@gmail.com>
2024-09-10 15:59:54 +0700

    add file

---
+++ script.js
@@ -0,0 +1,27 @@
+VANTA.RINGS({
+    el: "#f-ctr",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0x131313,
+    color: 0xff00
+})
+
+VANTA.CELLS({
+    el: "#c-ctr",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 3.50,
+    color1: 0xb6b6b6,
+    color2: 0x0,
+    size: 1.00,
+    speed: 1.10
+})
+
